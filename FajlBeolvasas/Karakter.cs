7f8ef0ac7a525e8e70319ecd98804701fbbe1912
Petrik Diak <diak@petrik.hu>
2025-02-14 11:23:40 +0100

    1. feladat és fájlbeolvasás kész

---
+++ FajlBeolvasas/Karakter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FajlBeolvasas
+{
+	internal class Karakter
+	{
+		private string nev;
+		private int szint;
+		private int eletero;
+		private int ero;
+
+		public Karakter(string nev, int szint, int eletero, int ero)
+		{
+			this.nev = nev;
+			this.szint = szint;
+			this.eletero = eletero;
+			this.ero = ero;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Szint { get => szint; set => szint = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Ero { get => ero; set => ero = value; }
+
+		public override string ToString()
+		{
+			return $"Név: {this.nev}, Szint: {this.szint}, Életerő: {this.eletero}, Erő: {this.ero}";
+		}
+	}
+}
--- FajlBeolvasas/Program.cs
+++ FajlBeolvasas/Program.cs
@@ -4,7 +4,30 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
+			Beolvasas("karakterek.txt", karakterek);
+
+			foreach (var item in karakterek)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		static void Beolvasas(string filenev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new (filenev);
+            //Console.WriteLine(sr.ReadToEnd());
+            //Console.WriteLine(sr.ReadLine());
+			sr.ReadLine();
+
+			while (sr.EndOfStream == false)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(';');
+
+				Karakter karakter = new Karakter(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
 		}
 	}
 }
